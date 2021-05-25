ba4aa4809215a6e8507f146c1012a447fde71a15
AntoninB4440 <barceloantonin@gmail.com>
2021-05-25 11:55:47 +0200

    change project

--- 05-ionic-project/src/app/app.component.ts
+++ 05-ionic-project/src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Conferences', url: '/folder/Conference', icon: 'today' },
-    { title: 'Session', url: '/folder/Session', icon: 'easel' },
-    { title: 'Presentateur', url: '/folder/Presentateur', icon: 'person' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
--- 05-ionic-project/src/app/app.module.ts
+++ 05-ionic-project/src/app/app.module.ts
@@ -6,10 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ConferenceComponent } from './conference/conference.component';
 
 @NgModule({
-  declarations: [AppComponent, ConferenceComponent],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
