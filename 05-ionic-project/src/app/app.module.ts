8a340008fe334e1f6b2f4c3a3a7acddd112b8c06
AntoninB4440 <barceloantonin@gmail.com>
2021-05-26 10:54:15 +0200

    reset project

--- 05-ionic-project/src/app/app.module.ts
+++ 05-ionic-project/src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SessionComponent } from './session/session.component';
-import { SessionDetailsComponent } from './session-details/session-details.component';
 
 @NgModule({
-  declarations: [AppComponent, SessionComponent, SessionDetailsComponent],
+  declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule,HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
